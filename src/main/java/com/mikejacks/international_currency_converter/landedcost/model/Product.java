1ec3a08d10843bc2460456976b09f52229cf55a9
Mike Jacks <mjacks4816@xmail.dixietech.edu>
2024-06-03 21:54:06 -0600

    add Product model

---
+++ src/main/java/com/mikejacks/international_currency_converter/landedcost/model/Product.java
@@ -0,0 +1,28 @@
+package com.mikejacks.international_currency_converter.landedcost.model;
+
+public class Product {
+    private String name;
+    private Double price;
+
+    public Product(String name, Double price) {
+        this.setName(name);
+        this.setPrice(price);
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
