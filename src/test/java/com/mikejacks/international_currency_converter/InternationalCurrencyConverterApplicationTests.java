3dee925d71f0be0464953ae8d0bf8eb63250156d
Mike Jacks <mjacks4816@xmail.dixietech.edu>
2024-06-03 21:43:19 -0600

    add tests

---
+++ src/test/java/com/mikejacks/international_currency_converter/InternationalCurrencyConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikejacks.international_currency_converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternationalCurrencyConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
